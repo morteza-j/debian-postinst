bdaaf238a973c2d4bbe4542fb0122db43b9f7270
Morteza J <jmorteza.me@gmail.com>
2026-08-02 22:56:22 +0330

    add debsecan package

--- debian-postinst.sh
+++ debian-postinst.sh
@@ -455,7 +455,7 @@ install_essential_packages() {
     info "Installing essential packages..."
 
     local packages=(
-        apt-offline conntrack python3 python3-pip age parted psmisc lsof file man man-db apt-utils vim tree less curl wget gnupg gpg ca-certificates lsb-release apt-transport-https git zip unzip bash-completion util-linux grub-common bsdextrautils mokutil htop iotop mtr iftop sysstat procps iproute2 bind9-dnsutils traceroute tcpdump iputils-ping bridge-utils iptables resolvconf firewalld smartmontools net-tools ncat gnupg2 sbsigntool tmux auditd unrar-free sudo rsync
+        apt-offline conntrack python3 python3-pip age parted psmisc lsof file man man-db apt-utils vim tree less curl wget gnupg gpg ca-certificates lsb-release apt-transport-https git zip unzip bash-completion util-linux grub-common bsdextrautils mokutil htop iotop mtr iftop sysstat procps iproute2 bind9-dnsutils traceroute tcpdump iputils-ping bridge-utils iptables resolvconf firewalld smartmontools net-tools ncat gnupg2 sbsigntool tmux auditd unrar-free sudo rsync debsecan
     )
 
     log "INFO" "Installing packages: ${packages[*]}"
